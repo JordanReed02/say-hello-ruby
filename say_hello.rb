8ad3c8e90dd4d5583c72eff27627a686d0ca77f0
Joran <jordanalireed@icloud.com>
2020-11-11 14:43:36 -0500

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,20 @@
-# Build your say_hello method here
+class Person
+end
+
+def say_hello
+  puts "Hello, Ruby programmer!"
+end
+
+def say_hello(name)
+  puts "Hello, #{name}"
+end
+
+#       assigning a default value
+def say_hello(name = "Ruby Programmer")
+  puts "Hello #{name}!"
+end
+
+jaquavious = Person.new
+jaquavious.object_id #=> 2
+
+say_hello(jaquavious)
